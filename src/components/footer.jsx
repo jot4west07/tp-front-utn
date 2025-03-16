d400fe7fea6318eeb2361504b441e7b61afbe7c8
Juan <wesrt5028@gmail.com>
2025-03-15 23:03:16 -0300

    Se mejora el footer y estilos css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,17 @@
 import "../styles/styles.css"; // Importamos los estilos
+import starwars3 from "../assets/images/star-wars3.jpg"
+import starwars4 from "../assets/images/star-wars4.png"
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <div>
+        <img src={starwars3} alt="Imagen star wars footer" />
+      </div>
       <p>Todos los derechos reservados ®</p>
+      <div>
+        <img src={starwars4} alt="Imagen star wars footer" />
+      </div>
     </footer>
   );
 };
